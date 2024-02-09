cf89ad5c9a17ab1c990cfe38f2d19507ad41d29a
jdencala <jdencalahuaya@gmail.com>
2024-02-09 12:27:31 -0500

    Add(II): Add age param to rest endpoints

--- src/main/java/com/ups/poo/controller/TestController.java
+++ src/main/java/com/ups/poo/controller/TestController.java
@@ -15,14 +15,20 @@ public class TestController {
 
     @GetMapping("/hello")
     public String hello(@RequestParam String name,
-                        @RequestParam String lastname){
-        return "This is my first SpringBootProject!, and my name is: "
-                + name + " " + lastname;
+                        @RequestParam String lastname,
+                        @RequestParam(required = false) Integer age){
+        String message = "This is my first SpringBootProject!" +
+                ", and my name is: "+ name + " " + lastname;
+        if(age != null) {
+             message = message + " and my age is " + age;
+        }
+        return message;
     }
 
-    @GetMapping("/concat/{name}/{lastname}")
-    public String concatenate(@PathVariable String name, @PathVariable String lastname){
+    @GetMapping("/concat/{name}/{lastname}/{age}")
+    public String concatenate(@PathVariable String name, @PathVariable String lastname,
+                              @PathVariable int age){
         return "This is my second rest service!, and my name is: "
-                + name + " " + lastname;
+                + name + " " + lastname + " and my age is " + age;
     }
 }
