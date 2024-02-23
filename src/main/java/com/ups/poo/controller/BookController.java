5e6bd1ec6e5ce9966affd61c4651a96ab31e44a8
jdencala <jdencalahuaya@gmail.com>
2024-02-23 13:28:33 -0500

    Add(v): Add Author and Book Controllers

--- src/main/java/com/ups/poo/controller/BookController.java
+++ src/main/java/com/ups/poo/controller/BookController.java
@@ -1,7 +1,28 @@
 package com.ups.poo.controller;
 
+import com.ups.poo.dto.BookDTO;
+import com.ups.poo.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping(value = "/all-books", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity findAllBooks() {
+        List<BookDTO> bookList = new ArrayList<>();
+        bookList = bookService.findAll();
+        return new ResponseEntity<>(bookList, HttpStatus.OK);
+    }
 }
--- src/main/java/com/ups/poo/service/BookService.java
+++ src/main/java/com/ups/poo/service/BookService.java
@@ -4,9 +4,12 @@ import com.ups.poo.domain.Book;
 import com.ups.poo.dto.AuthorDTO;
 import com.ups.poo.dto.BookDTO;
 import com.ups.poo.repository.BookRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class BookService {
     private final BookRepository bookRepository;
 
@@ -16,6 +19,7 @@ public class BookService {
 
     public List<BookDTO> findAll() {
         Iterable<Book> books = bookRepository.findAll();
+        List<BookDTO> bookList = new ArrayList<>();
         for(Book bookI : books) {
             BookDTO book = new BookDTO();
             book.setTitle(bookI.getTitle());
@@ -23,6 +27,9 @@ public class BookService {
             AuthorDTO authorDTO = new AuthorDTO();
             authorDTO.setName(bookI.getAuthor().getName());
             authorDTO.setLastName(bookI.getAuthor().getLastName());
+            book.setAuthorDTO(authorDTO);
+            bookList.add(book);
         }
+        return bookList;
     }
 }
